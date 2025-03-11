8c30df86fd333962640a46dee9836d564d536e37
조규용 <mvpcouple@naver.com>
2025-03-11 21:30:27 +0900

    hahah

--- client/src/pages/IdeaBoard/IdeaBorad.jsx
+++ client/src/pages/IdeaBoard/IdeaBorad.jsx
@@ -3,56 +3,56 @@ import "./ideaBorad.scss";
 import DataTable from "../../components/DataTable/DataTable";
 
 const IdeaBorad = () => {
-
   const columns = [
-    { field: 'id', headerName: 'ID', width: 90 },
+    { field: "id", headerName: "ID", width: 90 },
     {
-      field: 'firstName',
-      headerName: 'First name',
+      field: "firstName",
+      headerName: "First name",
       width: 150,
       editable: true,
     },
     {
-      field: 'lastName',
-      headerName: 'Last name',
+      field: "lastName",
+      headerName: "Last name",
       width: 150,
       editable: true,
     },
     {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
+      field: "age",
+      headerName: "Age",
+      type: "number",
       width: 110,
       editable: true,
     },
     {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
+      field: "fullName",
+      headerName: "Full name",
+      description: "This column has a value getter and is not sortable.",
       sortable: false,
       width: 160,
-      valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+      valueGetter: (value, row) =>
+        `${row.firstName || ""} ${row.lastName || ""}`,
     },
   ];
 
   const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, lastName: "Snow", firstName: "Jon", age: 14 },
+    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
+    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
   ];
 
   useEffect(() => {
-    const appElement = document.querySelector('.app');
-    appElement.classList.add('ideaBoard-active');
+    const appElement = document.querySelector(".app");
+    appElement.classList.add("ideaBoard-active");
 
     return () => {
-      appElement.classList.remove('ideaBoard-active');
+      appElement.classList.remove("ideaBoard-active");
     };
   }, []);
 
@@ -69,22 +69,16 @@ const IdeaBorad = () => {
           <div className="menuNm">Solution 등록</div>
           <div className="menuNm">진행률 관리</div>
           <div className="menuNm">과제 평가</div>
-
-
         </div>
         <div className="contents">
-          <h1>
-            Idea 등록 게시판 (Main 버전)
-          </h1>
+          <h1>Idea 등록 게시판 (Main 버전) haha</h1>
           <button>아이디어 등록</button>
           <DataTable slug="idea" columns={columns} rows={rows} />
         </div>
-
       </div>
       <div className="gap-60"></div>
     </div>
-  )
-}
+  );
+};
 
-
-export default IdeaBorad
+export default IdeaBorad;
