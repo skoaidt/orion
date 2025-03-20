784f37cea4f3a3e7d6353cb45ce233c0d773f504
조규용 <mvpcouple@naver.com>
2025-03-20 17:45:51 +0900

    0320_kyu_gantt2차

--- client/src/pages/IdeaBoard/IdeaGantt/IdeaGantt.jsx
+++ client/src/pages/IdeaBoard/IdeaGantt/IdeaGantt.jsx
@@ -4,10 +4,13 @@ import "./ideaGantt.scss";
 import { Gantt } from "wx-react-gantt";
 import { Willow } from "wx-react-gantt";
 import "wx-react-gantt/dist/gantt.css";
+import { Toolbar } from "wx-react-gantt";
+import { useRef } from "react";
 
 const IdeaGantt = () => {
   const { id } = useParams();
   const navigate = useNavigate();
+  const apiRef = useRef();
 
   const tasks = [
     {
@@ -94,6 +97,75 @@ const IdeaGantt = () => {
     { unit: "day", step: 1, format: "d" },
   ];
 
+  const zoomConfig = {
+    maxCellWidth: 400,
+    level: 3,
+    levels: [
+      {
+        minCellWidth: 200,
+        scales: [{ unit: "year", step: 1, format: "yyyy" }],
+      },
+      {
+        minCellWidth: 150,
+        scales: [
+          { unit: "year", step: 1, format: "yyyy" },
+          { unit: "quarter", step: 1, format: "QQQQ" },
+        ],
+      },
+      {
+        minCellWidth: 250,
+        scales: [
+          { unit: "quarter", step: 1, format: "QQQQ" },
+          { unit: "month", step: 1, format: "MMMM yyy" },
+        ],
+      },
+      {
+        minCellWidth: 100,
+        scales: [
+          { unit: "month", step: 1, format: "MMMM yyy" },
+          { unit: "week", step: 1, format: "'week' w" },
+        ],
+      },
+      {
+        maxCellWidth: 200,
+        scales: [
+          { unit: "month", step: 1, format: "MMMM yyy" },
+          { unit: "day", step: 1, format: "d" },
+        ],
+      },
+      {
+        minCellWidth: 25,
+        scales: [
+          { unit: "day", step: 1, format: "MMM d" },
+          { unit: "hour", step: 6, format: "HH:mm" },
+        ],
+      },
+      {
+        scales: [
+          { unit: "day", step: 1, format: "MMM d" },
+          { unit: "hour", step: 1, format: "HH:mm" },
+        ],
+      },
+    ],
+  };
+
+  const items = [
+    {
+      id: "add-task",
+      comp: "button",
+      icon: "wxi-plus",
+      text: "작업 추가",
+      type: "primary",
+    },
+    {
+      id: "edit-task",
+      comp: "button",
+      icon: "wxi-edit",
+      text: "편집",
+      type: "link",
+    },
+  ];
+
   const handleBackClick = () => {
     navigate(`/ideaboard/detail/${id}`);
   };
@@ -107,12 +179,15 @@ const IdeaGantt = () => {
         </button>
       </div>
       <div className="ganttContainer">
+        <Toolbar api={apiRef.current} items={items} />
         <Willow>
           <Gantt
+            apiRef={apiRef}
             tasks={tasks}
             columns={columns}
             links={links}
             scales={scales}
+            zoom={zoomConfig}
           />
         </Willow>
       </div>
