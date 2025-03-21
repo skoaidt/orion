2e71dc92bd8290559ce9969d279bc0d269ca7557
SKT\P107372 <minyoung.kim1310@gmail.com>
2025-03-21 16:21:47 +0900

    0321_min0_ideaPilot 프론트완성

--- client/src/pages/IdeaBoard/IdeaModal/IdeaPilot.jsx
+++ client/src/pages/IdeaBoard/IdeaModal/IdeaPilot.jsx
@@ -5,39 +5,69 @@ import TextField from "@mui/material/TextField";
 
 const IdeaPilot = ({ onClose }) => {
   return (
-    <div className="modalOverlay">
-      <div className="modalContent">
+    <div className="PilotModalOverlay">
+      <div className="PilotModalContent">
         <div className="titleBox">
           <h2>현장 Pilot 결과</h2>
           <CloseIcon className="closeIcon" onClick={onClose} />
         </div>
         <hr className="titleUnderline" />
-
-        <div className="rowContainerComment">
+        <div className="rowContainer">
           {/*생산성*/}
           <div className="rowContainerSmall">
             <div className="fieldLabel">생산성</div>
-            <TextField
-              className="commentTextfield"
-              variant="outlined"
-              //placeholder="의견을 작성해주세요"
-              //fullWidth
-            />
+            <TextField className="Textfield" variant="outlined" />
             <div className="fieldText">M/M</div>
           </div>
           {/*비용*/}
           <div className="rowContainerSmall">
             <div className="fieldLabel">비용</div>
-            <TextField
-              className="commentTextfield"
-              variant="outlined"
-              //placeholder="의견을 작성해주세요"
-              //fullWidth
-            />
-            <div className="fieldText">억원</div>
+            <TextField className="Textfield" variant="outlined" />
+            <div className="fieldText">억 원</div>
           </div>
         </div>
-        <button onClick={onClose}>닫기</button>
+        <div className="rowContainerBasis">
+          <div className="fieldLabel">
+            정량적
+            <br />
+            기대효과
+            <br />
+            근거
+          </div>
+          <TextField
+            className="Textfield"
+            variant="outlined"
+            multiline
+            rows={12}
+            fullWidth
+            defaultValue={`○ 생산성\n○ 비용`}
+          />
+        </div>
+        <div className="rowContainerResult">
+          <div className="fieldLabel">결과</div>
+          <TextField
+            className="Textfield"
+            variant="outlined"
+            //placeholder="결과를 업로드하세요"
+          />
+          <button className="uploadButton">
+            <img
+              src={`${process.env.PUBLIC_URL}/image/icons/upload-white.png`}
+              alt="업로드 아이콘"
+              className="uploadIcon"
+            />
+            첨부파일 upload
+          </button>
+        </div>
+        {/* 버튼 컨테이너 */}
+        <div className="buttonContainer">
+          <button className="cancelButton" onClick={onClose}>
+            취소
+          </button>
+          <button className="registerButton" onClick={onClose}>
+            등록
+          </button>
+        </div>
       </div>
     </div>
   );
