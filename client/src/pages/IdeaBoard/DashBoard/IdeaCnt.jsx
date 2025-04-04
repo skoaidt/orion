376c9cc6428d1d7cbc1856c6a0e07758a2632017
조규용 <mvpcouple@naver.com>
2025-04-04 15:28:52 +0900

    0404_kyu_아직작업중

--- client/src/pages/IdeaBoard/DashBoard/IdeaCnt.jsx
+++ client/src/pages/IdeaBoard/DashBoard/IdeaCnt.jsx
@@ -9,18 +9,21 @@ const IdeaCnt = () => {
   return (
     <div className="ideaCnt">
       <div className="header">
-        <span>IDEA 등록건수</span>
-        <span>기준 : {new Date().getFullYear() + 2}년 누적</span>
+        <span>IDEA 등록</span>
+        <span>기준 : {new Date().getFullYear()}년 누적</span>
       </div>
       <div className="totalCnt">
+        <div className="totalCntTitle">총 등록건수</div>
         <p>
           {ideaCnt.totalCnt.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
         </p>
       </div>
       <div className="ideaCntBox">
-        <div className="regBox">
+        <div className="box regBox">
           <div className="boxHeader">
-            <div className="icon"></div>
+            <div className="iconCircle">
+              <CreateIcon />
+            </div>
             <p>등록</p>
           </div>
           <div className="boxContent">
@@ -32,8 +35,38 @@ const IdeaCnt = () => {
             </p>
           </div>
         </div>
-        <div className="devBox"></div>
-        <div className="compBox"></div>
+        <div className="box devBox">
+          <div className="boxHeader">
+            <div className="iconCircle">
+              <WebhookIcon />
+            </div>
+            <p>개발중</p>
+          </div>
+          <div className="boxContent">
+            <p>
+              {ideaCnt.developing
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              건
+            </p>
+          </div>
+        </div>
+        <div className="box compBox">
+          <div className="boxHeader">
+            <div className="iconCircle">
+              <MilitaryTechIcon />
+            </div>
+            <p>완료</p>
+          </div>
+          <div className="boxContent">
+            <p>
+              {ideaCnt.completed
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              건
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
